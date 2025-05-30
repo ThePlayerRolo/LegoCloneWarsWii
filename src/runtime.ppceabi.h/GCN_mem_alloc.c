9c3225d5f2fc0679813c67ba069949c3c6659a4a
ThePlayerRolo <thesolis78243@gmail.com>
2025-05-30 15:04:19 -0400

    Mostly Matching Besides RegSwaps

--- src/runtime.ppceabi.h/GCN_mem_alloc.c
+++ src/runtime.ppceabi.h/GCN_mem_alloc.c
@@ -1,9 +1,9 @@
+/* Originally from Petari: https://github.com/SMGCommunity/Petari */
 extern void OSReport(const char *, ...);
 extern void* OSGetMEM1ArenaLo();
 extern void* OSGetMEM1ArenaHi();
 extern void* OSInitAlloc(void *, void *, int);
 extern volatile int __OSCurrHeap;
-extern void* OSAllocFromHeap(int, int);
 #define OSRoundUp32B(x) (((unsigned int)(x) + 32 - 1) & ~(32 - 1))
 #define OSRoundDown32B(x) (((unsigned int)(x)) & ~(32 - 1))
 
@@ -32,4 +32,27 @@ void __sys_alloc(int a) {
 
     OSAllocFromHeap(__OSCurrHeap, a);
 
+}
+
+__declspec(weak) extern void __sys_free(void* ptr)
+{
+    void* arenaLo;
+	void* arenaHi;
+    if (__OSCurrHeap == -1) {
+        OSReport("GCN_Mem_Alloc.c : InitDefaultHeap. No Heap Available\n");
+	    OSReport("Metrowerks CW runtime library initializing default heap\n");
+
+        arenaLo = OSGetMEM1ArenaLo();
+	    arenaHi = OSGetMEM1ArenaHi();
+
+	    arenaLo = OSInitAlloc(arenaLo, arenaHi, 1);
+	    OSSetMEM1ArenaLo(arenaLo);
+
+        arenaLo = (void*)OSRoundUp32B(arenaLo);
+	    arenaHi = (void*)OSRoundDown32B(arenaHi);
+
+	    OSSetCurrentHeap(OSCreateHeap(arenaLo, arenaHi));
+	    OSSetMEM1ArenaLo(arenaLo = arenaHi);
+    }
+    OSFreeToHeap(__OSCurrHeap, ptr);
 }
