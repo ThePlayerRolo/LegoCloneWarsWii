3decd0f86505140d16a5b574ddf3e3dbb51a7ef9
ThePlayerRolo <thesolis78243@gmail.com>
2025-06-20 11:47:04 -0400

    more work on NuMath's Random Functions

--- src/numath.master/src/NuRand.cpp
+++ src/numath.master/src/NuRand.cpp
@@ -2,8 +2,10 @@
 
 
 static unsigned int fseed;
+//Todo: Figure out what generates floatbase0.
+static float floatBase0;
 
-float NuRandFloat();
+
 
 
 
@@ -13,6 +15,19 @@ void NuRandSeed(unsigned int param1) {
 }
 unsigned int NuRandGetSeed() {
     return fseed;
+}
+
+float NuRandFloatSeeded(unsigned int* param1) {
+    //floatBase0 = 4;
+    unsigned int uVar1 = *param1 * 0x19660d + 0x3c6ef35f;
+    *param1 = uVar1;
+    uVar1 &= 0x7fffff;
+    uVar1|= 0x3f800000;
+    return *(float*)&uVar1 - floatBase0;
+}
+
+float NuRandFloat() {
+    NuRandFloatSeeded(&fseed);
 }
 
 unsigned int NuRandIntSeeded(unsigned int* param1) {
